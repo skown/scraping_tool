dadd7fa91e3270c9e1b97f1ea54088390274b834
Adrian Skowron <38570801+skown@users.noreply.github.com>
2023-07-04 09:19:35 +0200

    Update serp_results_1.py

--- dfs_scripts/serp_results_1.py
+++ dfs_scripts/serp_results_1.py
@@ -46,32 +46,4 @@ def Post_desktop_data(keywords):
 		response = client.post("/v3/serp/google/organic/task_post", post_data)
 		print(response)
 		post_data.clear()
-'''
-if response["status_code"] == 20000:
-	for task_number in range(len(response['tasks'])):
-		for tasks, task_value in response['tasks'][task_number].items():
-			if tasks == 'id':
-				for keyword, keyword_value in response['tasks'][task_number]['data'].items():
-					if keyword == 'keyword':
-						print(task_number)
-						print(task_value, '->', keyword_value)
-else:
-	print("error. Code: %d Message: %s" % (response["status_code"], response["status_message"]))
-'''
 
-'''
-response = client.post("/v3/serp/google/organic/task_post", post_data)
-if response["status_code"] == 20000:
-    print(response)
-    # do something with result
-else:
-    print("error. Code: %d Message: %s" % (response["status_code"], response["status_message"]))
-'''
-
-'''
-update = dict()
-keywords = ['brainly', 'brainly app', 'brainly page']
-for u in keywords:
-	update = dict(keyword=u)
-	print(update)
-'''
